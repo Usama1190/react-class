020d47cf2ec3352f13c57c33d0a84e55bcf16267
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-08-30 12:08:52 -0700

    Keep learning on Aug 31, 24

---
+++ app/components/Gallery.tsx
@@ -0,0 +1,24 @@
+// import Profile from "./Profile";
+
+export function Profile() {           // named export
+    return (
+        <img
+          src="https://i.imgur.com/MK3eW3Am.jpg"
+          alt="Katherine Johnson"
+        />
+      )
+}
+
+export default function Gallery() {
+  return (
+    <div>
+      <h1>Amazing scientists</h1>
+
+      <div className="flex">
+        <Profile />
+        <Profile />
+        <Profile />
+      </div>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,48 @@
 import Image from "next/image";
 
-function Profile() {
-  return (
-    <img
-      src="https://i.imgur.com/MK3eW3Am.jpg"
-      alt="Katherine Johnson"
-    />
-  )
-}
+// function Profile() {
+//   return (
+//     <img
+//       src="https://i.imgur.com/MK3eW3Am.jpg"
+//       alt="Katherine Johnson"
+//     />
+//   )
+// }
 
 
-export default function Gallery() {
+// export default function Gallery() {
+//   return (
+//     <div>
+//       <h1>Amazing scientists</h1>
+
+//       <div className="flex">
+//         <Profile />
+//         <Profile />
+//         <Profile />
+//       </div>
+//     </div>
+//   )
+// }
+
+
+// import Gallery from "./components/Gallery";
+
+// export default function Home() {
+//   return (
+//     <Gallery />
+//   )
+// }
+
+
+
+import Gallery from "./components/Gallery";
+import { Profile } from "./components/Gallery";    // named export
+
+export default function Home() {
   return (
     <div>
-      <h1>Amazing scientists</h1>
-
-      <div className="flex">
-        <Profile />
-        <Profile />
-        <Profile />
-      </div>
+      {/* <Gallery /> */}
+      <Profile />
     </div>
   )
 }
