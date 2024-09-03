b9107d6090e2b174938ab330801d0c0641f3e3da
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-09-03 12:16:58 -0700

    Keep learning on Sep 4th, 24

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -1,6 +1,19 @@
-export default function Form({
-    status = 'empty'
-  }) {
+'use client';
+
+import { useState } from "react";
+
+export default function Form() {
+    const [answer, setAnswer] = useState('');
+    const [error, setError] = useState(null);
+
+    const [status, setStatus] = useState('typing'); // 'typing', 'submitting', or 'success'
+
+    function handleTextareaChange(e: any) {
+      setAnswer(e.target.value);
+      console.log(answer);
+      
+    }
+
     if (status === 'success') {
       return <h1>That&apos;s right!</h1>
     }
@@ -11,11 +24,23 @@ export default function Form({
           In which city is there a billboard that turns air into drinkable water?
         </p>
         <form>
-          <textarea />
+          <textarea 
+          disabled={status === 'submitting'}
+          value={answer}
+          onChange={handleTextareaChange}  />
           <br />
-          <button>
+          <button disabled={
+          status === 'empty' ||
+          status === 'submitting'
+        }>
             Submit
           </button>
+
+          {status === 'error' &&
+          <p className="Error">
+            Good guess but a wrong answer. Try again!
+          </p>
+        }
         </form>
       </>
     )
